e4247274ec959314d1fd1e9de5f055022bc603a7
Syed Zaman Mostafiz <zamandiue44>
2017-02-10 00:35:31 +0600

    HTML5&CSS3V01 is Complete

---
+++ LICTProjectClass/E004D09Feb17.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+	<title>This is Title</title>
+	<style type="text/css">
+	.first{
+		background-color:black; 
+		color:white;
+		padding: 20px;
+		margin:20px;
+	}
+	</style>
+	
+	</head>
+	
+	<body>
+	<div class="first">
+	header
+	</div>
+	<div class="first">
+	body
+	</div>
+	<a href="E004D09Feb17.php" target="_blank">Exercise004ClickHere</a>
+
+</html>
